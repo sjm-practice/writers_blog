ee0ce5e57ce16552bd1299f72bc851835488e2b7
stevenjmarsh <sjm.atl@gmail.com>
2018-10-01 11:27:29 -0500

    formatting writer component

--- imports/ui/Writers/Writer/index.jsx
+++ imports/ui/Writers/Writer/index.jsx
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
 
-export default ({ id, name, description, image }) => (
+export default ({ name, born, deceased, description, image }) => (
   <Fragment>
-    <div>
-      {id}
-      {name}
-      {description}
-      {image}
-    </div>
+    <img src={image} alt={name} style={{ maxWidth: 300 }} />
+    <h1>{name}</h1>
+    <h3>
+      {born} &mdash; {deceased}
+    </h3>
+    <p>{description}</p>
   </Fragment>
 );
